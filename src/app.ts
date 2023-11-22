31b92b1e4f003d3edbafd05350bd2d5e89f30f69
engrkashem <kashemaust@gmail.com>
2023-11-22 22:25:25 +0600

    mongoose connection added

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,9 @@ import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.json({
+    greet: 'Hello World',
+  });
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,19 @@
 import app from './app';
 import config from './app/config';
+import mongoose from 'mongoose';
 
-app.listen(config.port, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Example app listening on port ${config.port}`);
-});
+async function server() {
+  try {
+    await mongoose.connect(config.db_url as string);
+
+    app.listen(config.port, () => {
+      // eslint-disable-next-line no-console
+      console.log(`Assignment2 app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.log(err);
+  }
+}
+
+server();
