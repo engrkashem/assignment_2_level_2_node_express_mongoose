c2e60b0e0b9600bb1b21dcda9b5790211afb07a5
engrkashem <kashemaust@gmail.com>
2023-11-23 21:36:43 +0600

    password is hided before sending response to client. schema toJSON and transform method is used

--- src/app/mudules/users/users.model.ts
+++ src/app/mudules/users/users.model.ts
@@ -48,44 +48,66 @@ const orderSchema = new Schema<TOrder>({
 });
 
 // main Schema
-export const userSchema = new Schema<TUser>({
-  userId: {
-    type: Number,
-    required: [true, 'User ID is required'],
-    unique: true,
+export const userSchema = new Schema<TUser>(
+  {
+    userId: {
+      type: Number,
+      required: [true, 'User ID is required'],
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: [true, 'User name is required'],
+      trim: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      trim: true,
+      maxlength: [20, 'Last name must be less than 20 characters'],
+      minlength: [3, 'Password is too short. min 3 characters is needed.'],
+    },
+    fullName: {
+      type: fullNameSchema,
+      required: [true, 'Full name is required'],
+    },
+    age: { type: Number, required: [true, 'Age is required'] },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      trim: true,
+      match: [emailRegEx, '{VALUE} is not valid email address.'],
+    },
+    isActive: { type: Boolean, default: true },
+    hobbies: [{ type: String }],
+    address: { type: addressSchema, required: [true, 'Address is required'] },
+    orders: [{ type: orderSchema }],
   },
-  username: {
-    type: String,
-    required: [true, 'User name is required'],
-    trim: true,
-    unique: true,
+  {
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.password;
+      },
+    },
   },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    trim: true,
-    maxlength: [20, 'Last name must be less than 20 characters'],
-    minlength: [3, 'Password is too short. min 3 characters is needed.'],
-  },
-  fullName: { type: fullNameSchema, required: [true, 'Full name is required'] },
-  age: { type: Number, required: [true, 'Age is required'] },
-  email: {
-    type: String,
-    required: [true, 'Email is required'],
-    trim: true,
-    match: [emailRegEx, '{VALUE} is not valid email address.'],
-  },
-  isActive: { type: Boolean, default: true },
-  hobbies: [{ type: String }],
-  address: { type: addressSchema, required: [true, 'Address is required'] },
-  orders: [{ type: orderSchema }],
-});
+);
 
+/********** Middlewares **********/
+
+// pre middleware to save
 userSchema.pre('save', async function (next) {
   // const user=this;
   this.password = await bcrypt.hash(this.password, Number(config.saltRounds));
   next();
 });
+
+// pre middleware to save
+// userSchema.post('save', async function (userData, next) {
+//   // delete userData['password'];
+
+//   next();
+// });
 
 /********** Model (Schema) **********/
 export const User = model<TUser>('User', userSchema);
